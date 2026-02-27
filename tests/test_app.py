5ec4084f86d99c23c06a07633e7a48bee7af6578
Mariyah Rohim <mariyah.rohim@bankofengland.co.uk>
2026-02-27 19:50:21 +0000

    back to passing test

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from app import percentage
 
 class TestQuizLogic(unittest.TestCase):
     def test_calculate_percentage(self):
-        self.assertEqual(percentage(4, 8), 60.00)
+        self.assertEqual(percentage(4, 8), 50.00)
 
 if __name__ == "__main__":
     unittest.main()
