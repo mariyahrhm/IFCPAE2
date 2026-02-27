ed9d68c2fdb052d4e2ae57fa2f2495a3580ae7a1
Mariyah Rohim <mariyah.rohim@bankofengland.co.uk>
2026-02-27 19:27:19 +0000

    unittest failing

--- tests/unittest.py
+++ tests/unittest.py
@@ -5,10 +5,10 @@ from app import percentage
 class TestQuizLogic(unittest.TestCase):
 
     def test_calculate_percentage_basic(self):
-        self.assertEqual(percentage(4, 8), 50.0)
+        self.assertEqual(percentage(3, 8), 10.0)
 
     def test_calculate_percentage_zero_total(self):
-        self.assertEqual(percentage(0, 0), 0.0)
+        self.assertEqual(percentage(0, 0), 100.0)
 
 if __name__ == "__main__":
     unittest.main()
